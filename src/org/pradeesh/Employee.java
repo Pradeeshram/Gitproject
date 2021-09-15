eeffe6325dc4eec07a185345ea9ee8d2d42e4ffe
pradeeshram <pradeeshram220@gmail.com>
2021-09-15 16:05:18 +0530

    message

---
+++ src/org/pradeesh/Employee.java
@@ -0,0 +1,28 @@
+package org.pradeesh;
+
+public class Employee {
+
+	public void EmpId() {
+		
+		System.out.println("Emp id is 23456");
+		
+		
+	}		
+		
+		public static void main(String[] args) {
+			
+			Employee e = new Employee();
+			e.EmpId();
+			
+			
+		}
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+
